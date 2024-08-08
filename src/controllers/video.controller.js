bbb622973ab9809ef237c8b494e9831bb5555e2c
firdoush <mdfirdoushali1776@gmail.com>
2024-08-08 17:57:03 +0530

    toggle publish status of a video

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -401,9 +401,44 @@ const deleteVideo = asyncHandler(async (req, res) => {
     
 })
 
+// toggle publish status of a video
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
-})
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid VideoId")
+    }
+
+    const video = await Video.findById(videoId)
+
+    if (!video) {
+        throw new ApiError(404, "Video not found"); 
+    }
+
+    if (video?.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(400, "Only the owner can toggle the publish status")
+    }
+
+    const toggledVideoPublish = await Video.findByIdAndUpdate(videoId,{
+        $set: {
+            isPublished: !video?.isPublished
+        }
+    },{ new : true})
+
+    if (!toggledVideoPublish) {
+        throw new ApiError(500, "Failed to toogle video publish status");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { isPublished: toggledVideoPublish.isPublished },
+                "Video publish toggled successfully"
+            )
+        );
+});
 
 export {
     getAllVideos,
