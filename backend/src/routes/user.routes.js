f34d802028c93909b688d056d634eda633d5519d
firdoush <mdfirdoushali1776@gmail.com>
2024-10-24 22:49:31 +0530

    debugging

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -41,7 +41,7 @@ router.route("/current-user").get(verifyJWT, getCurrentUser);
 
 router.route("/update-user").patch(upload.none(), verifyJWT, updateUserDetails);
 router.route("/update-avatar").patch(verifyJWT, upload.single('avatar'), updateUserAvatar);
-router.route("/update-coverImg").patch(verifyJWT, upload.single('coverImage'), updateUserCoverImage);
+router.route("/update-coverImage").patch(verifyJWT, upload.single('coverImage'), updateUserCoverImage);
 
 router.route("/c/:username").get(verifyJWT, getUserChanelProfile);
 router.route("/watch-history").get(verifyJWT, getWatchHistory);
